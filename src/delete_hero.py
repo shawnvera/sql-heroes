ae5664ec4838aac340db7291f7b815b3336c522a
Shawn <134296052+shawnvera@users.noreply.github.com>
2023-07-20 17:11:45 -0400

    update

--- src/delete_hero.py
+++ src/delete_hero.py
@@ -2,13 +2,13 @@ from database.connection import execute_query, create_connection
 
 # Create a new hero in the DB
 
-delete_a_hero = input("What is the name of the hero you want to delete: ")
 
-def delete_hero(delete_a_hero):
-    
+
+def delete_hero():
+    delete_a_hero = input("What is the name of the hero you want to delete: ")
     query = """
         DELETE FROM heroes WHERE name = (%s);
         """
-    execute_query(query, delete_a_hero)
+    execute_query(query, (delete_a_hero,))
 
-delete_hero(delete_a_hero)
+delete_hero()
